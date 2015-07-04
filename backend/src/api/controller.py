f431fb96c3ba641a731166a32be0c07d19975d0e
amoranzea <amoranzea@gmail.com>
2015-07-04 20:14:08 +1000

    add cross-origin to the controller

--- backend/src/api/controller.py
+++ backend/src/api/controller.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, json
+from flask.ext.cors import CORS
 from handler import gmaps
 import requests
 app = Flask(__name__)
+CORS(app, resources=r'/api/*', allow_headers='Content-Type')
 
 
 @app.route("/aggregate", methods=['POST'])
