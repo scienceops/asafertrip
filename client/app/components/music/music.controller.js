7e2ab5785e14d7898096a3b0b4ddffe6f4e882bf
amoranzea <amoranzea@gmail.com>
2015-07-04 20:12:51 +1000

    Added progress bar

--- client/app/components/music/music.controller.js
+++ client/app/components/music/music.controller.js
@@ -2,18 +2,20 @@
     'use strict';
 
 	angular.module('asafertrip')
-		.controller('MusicController', ['$scope', 'musicServices', 'audioBufferServices', '$timeout', function ($scope, musicServices, audioBufferServices, $timeout) {
+		.controller('MusicController', ['$scope', 'musicServices', 'audioBufferServices', '$timeout', '$interval', function ($scope, musicServices, audioBufferServices, $timeout, $interval) {
 
+			var progress;
             var init = function()  {
             	$scope.sentences = [];
         		$scope.locations = {};
 				$scope.musics= musicServices.getData();
+				$scope.increaseWidth = 0;
 
 				audioBufferServices.playSound($scope.musics);
-
 				sequenceSentences($scope.musics);
+				increaseProgressBar();
+				myStopFunction();
             };
-
 
             var sequenceSentences = function (sources) {
 				 sources.forEach(function (sourceWrapper) {
@@ -23,6 +25,17 @@
 				 });
 			 }
 
+			var increaseProgressBar = function(){
+                progress = $interval(function () {
+					$scope.increaseWidth += 0.36;
+                }, 100);
+            };
+
+			var myStopFunction = function(){
+				$timeout(function(){
+					clearInterval(progress);
+				}, 30000);
+			};
 
 			init();
 
